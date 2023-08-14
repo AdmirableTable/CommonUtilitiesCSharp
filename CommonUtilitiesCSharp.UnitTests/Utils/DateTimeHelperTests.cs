472d6edffff6ba21c0205500d4c3ee0691385df3
David Cardinal <david.cardinal235@gmail.com>
2023-08-14 01:49:29 -0400

    Adds the DateTimeHelper class

---
+++ CommonUtilitiesCSharp.UnitTests/Utils/DateTimeHelperTests.cs
@@ -0,0 +1,82 @@
+﻿using CommonUtilitiesCSharp.Utils;
+
+namespace CommonUtilitiesCSharp.UnitTests.Utils
+{
+    public class DateTimeHelperTests
+    {
+        #region Get12HFormatHour
+        [Test]
+        public void Get12HFormatHour_Throws_ForInvalidHour()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<ArgumentException>(() => DateTimeHelper.Get12HFormatHour(-1));
+                Assert.Throws<ArgumentException>(() => DateTimeHelper.Get12HFormatHour(24));
+                Assert.Throws<ArgumentException>(() => DateTimeHelper.Get12HFormatHour(25));
+            });
+        }
+
+        [Test]
+        public void Get12HFormatHour_Returns12AM_For0()
+        {
+            Assert.That(DateTimeHelper.Get12HFormatHour(0), Is.EqualTo((12, false)));
+        }
+
+        [Test]
+        public void Get12HFormatHour_Returns12PM_For12()
+        {
+            Assert.That(DateTimeHelper.Get12HFormatHour(12), Is.EqualTo((12, true)));
+        }
+
+        [Test]
+        public void Get12HFormatHour_ReturnsExpected_ForValues()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(DateTimeHelper.Get12HFormatHour(1), Is.EqualTo((1, false)));
+                Assert.That(DateTimeHelper.Get12HFormatHour(6), Is.EqualTo((6, false)));
+                Assert.That(DateTimeHelper.Get12HFormatHour(13), Is.EqualTo((1, true)));
+                Assert.That(DateTimeHelper.Get12HFormatHour(18), Is.EqualTo((6, true)));
+            });
+        }
+        #endregion Get12HFormatHour
+
+        #region Get24HFormatHour
+        [Test]
+        public void Get24HFormatHour_Throws_ForInvalidHour()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.Throws<ArgumentException>(() => DateTimeHelper.Get24HFormatHour(-1, true));
+                Assert.Throws<ArgumentException>(() => DateTimeHelper.Get24HFormatHour(0, true));
+                Assert.Throws<ArgumentException>(() => DateTimeHelper.Get24HFormatHour(13, true));
+                Assert.Throws<ArgumentException>(() => DateTimeHelper.Get24HFormatHour(24, true));
+            });
+        }
+
+        [Test]
+        public void Get24HFormatHour_Returns0_For12AM()
+        {
+            Assert.That(DateTimeHelper.Get24HFormatHour(12, false), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Get24HFormatHour_Returns12_For12PM()
+        {
+            Assert.That(DateTimeHelper.Get24HFormatHour(12, true), Is.EqualTo(12));
+        }
+
+        [Test]
+        public void Get24HFormatHour_ReturnsExpected_ForValues()
+        {
+            Assert.Multiple(() =>
+            {
+                Assert.That(DateTimeHelper.Get24HFormatHour(1, false), Is.EqualTo(1));
+                Assert.That(DateTimeHelper.Get24HFormatHour(6, false), Is.EqualTo(6));
+                Assert.That(DateTimeHelper.Get24HFormatHour(1, true), Is.EqualTo(13));
+                Assert.That(DateTimeHelper.Get24HFormatHour(6, true), Is.EqualTo(18));
+            });
+        }
+        #endregion Get24HFormatHour
+    }
+}
---
+++ CommonUtilitiesCSharp/Utils/DateTimeHelper.cs
@@ -0,0 +1,58 @@
+﻿namespace CommonUtilitiesCSharp.Utils
+{
+    /// <summary>
+    /// Helper class containing methods for interacting with <see cref="DateTime"/> objects.
+    /// </summary>
+    public static class DateTimeHelper
+    {
+        /// <summary>
+        /// Returns the hour in 12-hour format and whether it is AM or PM.
+        /// </summary>
+        /// <param name="hour">24-hour value to convert.</param>
+        /// <returns>Returns the given 24-hour value into 12-hour format and a boolean that will be true if the hour is PM.</returns>
+        /// <exception cref="ArgumentException">Thrown is the given <paramref name="hour"/> parameter is not a 24-hour value. (0 to 23 inclusively)</exception>
+        public static (int Hour, bool IsPM) Get12HFormatHour(int hour)
+        {
+            if (hour < 0 || hour > 23)
+            {
+                throw new ArgumentException("Hour must be between 0 and 23, inclusively.", nameof(hour));
+            }
+
+            if (hour == 0)
+            {
+                return (12, false);
+            }
+
+            if (hour == 12)
+            {
+                return (12, true);
+            }
+
+            return (hour % 12, hour > 12);
+        }
+
+        /// <summary>
+        /// Returns the hour in 24-hour format.
+        /// </summary>
+        /// <param name="hour">12-hour value to convert.</param>
+        /// <param name="isPM">Set this parameter to true if the hour to convert is PM or false if the hour is AM.</param>
+        /// <returns>Returns the given 12-hour value into 24-hour format.</returns>
+        /// <exception cref="ArgumentException">Thrown if the given <paramref name="hour"/> parameter is not a 12-hour value. (1 to 12 inclusively)</exception>
+        public static int Get24HFormatHour(int hour, bool isPM)
+        {
+            if (hour < 1 || hour > 12)
+            {
+                throw new ArgumentException("Hour must be between 1 and 12, inclusively.", nameof(hour));
+            }
+
+            if (isPM)
+            {
+                return hour == 12 ? 12 : hour + 12;
+            }
+            else
+            {
+                return hour == 12 ? 0 : hour;
+            }
+        }
+    }
+}
