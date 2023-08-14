91dfce56a317068f87051481e80ea2ba64d9bca7
David Cardinal <david.cardinal235@gmail.com>
2023-08-13 23:02:11 -0400

    Adds string extensions

---
+++ CommonUtilitiesCSharp.UnitTests/Extensions/StringExtensionTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Data;
+
+namespace CommonUtilitiesCSharp.UnitTests.Extensions
+{
+    public class StringExtensionTests
+    {
+        #region Truncate
+        [Test]
+        public void Truncate_ReturnsCorrectResult_ForLongString()
+        {
+            var result = "1234567890".Truncate(5);
+            Assert.That(result, Is.EqualTo("12345"));
+        }
+
+        [Test]
+        public void Truncate_ReturnsCorrectResult_ForShortString()
+        {
+            var result = "12345".Truncate(10);
+            Assert.That(result, Is.EqualTo("12345"));
+        }
+
+        [Test]
+        public void Truncate_ReturnsCorrectResult_ForEmptyString()
+        {
+            var result = string.Empty.Truncate(10);
+            Assert.That(result, Is.EqualTo(string.Empty));
+        }
+
+#nullable disable
+        [Test]
+        public void Truncate_ReturnsCorrectResult_ForNullString()
+        {
+            string str = null;
+            var result = str.Truncate(10);
+            Assert.That(result, Is.Null);
+        }
+#nullable enable
+        #endregion Truncate
+    }
+}
---
+++ CommonUtilitiesCSharp/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Data;
+
+namespace CommonUtilitiesCSharp.UnitTests.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string str, int maxLength)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+
+            return str.Length <= maxLength ? str : str[..maxLength];
+        }
+    }
+}
