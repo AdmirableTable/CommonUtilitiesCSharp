660019fdf77cc7eb94b8c72eedee66704e0a45c7
David Cardinal <david.cardinal235@gmail.com>
2023-08-14 04:45:22 -0400

    Adds the 'ObservableObject' utility class

---
+++ CommonUtilitiesCSharp.UnitTests/Core/ObservableObjectTests.cs
@@ -0,0 +1,75 @@
+﻿using CommonUtilitiesCSharp.Core;
+using System.Runtime.CompilerServices;
+
+namespace CommonUtilitiesCSharp.UnitTests.Core
+{
+    public class ObservableObjectTests
+    {
+        private class MockObject : ObservableObject
+        {
+            public MockObject(string value)
+            {
+                _value = value;
+            }
+
+            private string _value;
+            public string Value
+            {
+                get => _value;
+                set
+                {
+                    _value = value;
+                    OnPropertyChanged();
+                }
+            }
+
+            public bool HasPropertyChangedBeenCalled { get; private set; }
+
+            protected override void OnPropertyChanged([CallerMemberName] string propertyName = "")
+            {
+                HasPropertyChangedBeenCalled = true;
+                base.OnPropertyChanged(propertyName);
+            }
+        }
+
+        [Test]
+        public void OnPropertyChanged_CanBeOverriden()
+        {
+            var mockObject = new MockObject("test");
+
+            Assert.That(mockObject.HasPropertyChangedBeenCalled, Is.False);
+
+            mockObject.Value = "test2";
+
+            Assert.That(mockObject.HasPropertyChangedBeenCalled, Is.True);
+        }
+
+        [Test]
+        public void OnPropertyChanged_RaisesPropertyChangedEvent()
+        {
+            var mockObject = new MockObject("test");
+            var flag = false;
+            var propertyName = "";
+
+            mockObject.PropertyChanged += (sender, args) =>
+            {
+                flag = true;
+                propertyName = args.PropertyName;
+            };
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(flag, Is.False);
+                Assert.That(propertyName, Is.EqualTo(""));
+            });
+
+            mockObject.Value = "test2";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(flag, Is.True);
+                Assert.That(propertyName, Is.EqualTo(nameof(MockObject.Value)));
+            });
+        }
+    }
+}
---
+++ CommonUtilitiesCSharp/Core/ObservableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace CommonUtilitiesCSharp.Core
+{
+    /// <summary>
+    /// Base class for objects that need to notify when their properties change. (Implementation of INotifyPropertyChanged)
+    /// </summary>
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        /// <inheritdoc cref="INotifyPropertyChanged.PropertyChanged"/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Triggers the PropertyChanged event for the given property name or the calling member name if no property name is given.
+        /// </summary>
+        /// <param name="propertyName">Name of the property used to raise the <see cref="PropertyChanged"/> event.</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
