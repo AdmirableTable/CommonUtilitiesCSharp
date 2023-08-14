c2cdb954244603fbb2879842e1e97add4ec157cb
David Cardinal <david.cardinal235@gmail.com>
2023-08-14 04:33:16 -0400

    Adds the 'Command' utility class

---
+++ CommonUtilitiesCSharp.UnitTests/Core/CommandTests.cs
@@ -0,0 +1,387 @@
+﻿using CommonUtilitiesCSharp.Core;
+
+namespace CommonUtilitiesCSharp.UnitTests.Core
+{
+    public abstract class CommandTestBase<TCommandParameter>
+    {
+        protected abstract Command CreateCommand(Action<TCommandParameter?> execute);
+
+        protected abstract Command CreateCommand(Action<TCommandParameter?> execute, Predicate<TCommandParameter?> canExecute);
+
+        #region Constructor
+#nullable disable
+        [Test]
+        public void Constructor_Throws_ForNullExecute()
+        {
+            Assert.Throws<ArgumentNullException>(() => CreateCommand(null));
+        }
+
+        [Test]
+        public void Constructor_Throws_ForNullCanExecute()
+        {
+            static void execute(TCommandParameter param) { }
+
+            Assert.Throws<ArgumentNullException>(() => CreateCommand(execute, null));
+        }
+
+        [Test]
+        public void Constructor_Throws_ForNullExecuteWithPredicate()
+        {
+            static bool canExecute(TCommandParameter param) { throw new NotSupportedException(); }
+
+            Assert.Throws<ArgumentNullException>(() => CreateCommand(null, canExecute));
+        }
+#nullable enable
+        #endregion Constructor
+
+        #region Execute
+        [Test]
+        public abstract void Execute_IsInvoked_WhenThereIsNoPredicate();
+
+        [Test]
+        public abstract void Execute_IsNotInvoked_WhenPredicateIsFalse();
+
+        [Test]
+        public abstract void Execute_IsInvoked_WhenPredicateIsTrue();
+
+        [Test]
+        public abstract void Execute_IsInvokedWithParameter_WhenParameterIsPassed();
+
+        [Test]
+        public abstract void Execute_IsInvokedWithParameter_WhenParameterIsPassedAndPredicateIsTrue();
+        #endregion Execute
+
+        #region CanExecute
+        [Test]
+        public abstract void CanExecute_ReturnsTrue_WhenThereIsNoPredicate();
+
+        [Test]
+        public abstract void CanExecute_ReturnsFalse_WhenPredicateIsFalse();
+
+        [Test]
+        public abstract void CanExecute_ReturnsTrue_WhenPredicateIsTrue();
+
+        [Test]
+        public abstract void CanExecute_ReturnsFalse_WhenParameterIsPassedAndPredicateIsFalse();
+
+        [Test]
+        public abstract void CanExecute_ReturnsTrue_WhenParameterIsPassedAndPredicateIsTrue();
+        #endregion CanExecute
+    }
+
+    public class CommandTests : CommandTestBase<object>
+    {
+        protected override Command CreateCommand(Action<object?> execute) => new(execute);
+
+        protected override Command CreateCommand(Action<object?> execute, Predicate<object?> canExecute) => new(execute, canExecute);
+
+        #region Execute
+        [Test]
+        public override void Execute_IsInvoked_WhenThereIsNoPredicate()
+        {
+            var flag = false;
+            var command = CreateCommand(parameter => flag = true);
+
+            Assert.That(flag, Is.False);
+
+            command.Execute();
+
+            Assert.That(flag, Is.True);
+        }
+
+        [Test]
+        public override void Execute_IsNotInvoked_WhenPredicateIsFalse()
+        {
+            var flag = false;
+            var condition = false;
+            var command = CreateCommand(parameter => flag = true, parameter => condition);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(flag, Is.False);
+                Assert.That(condition, Is.False);
+            });
+
+            command.Execute();
+
+            Assert.That(flag, Is.False);
+        }
+
+        [Test]
+        public override void Execute_IsInvoked_WhenPredicateIsTrue()
+        {
+            var flag = false;
+            var condition = false;
+            var command = CreateCommand(parameter => flag = true, parameter => condition);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(flag, Is.False);
+                Assert.That(condition, Is.False);
+            });
+
+            condition = true;
+            command.Execute();
+
+            Assert.That(flag, Is.True);
+        }
+
+#nullable disable
+        [Test]
+        public override void Execute_IsInvokedWithParameter_WhenParameterIsPassed()
+        {
+            var flag = false;
+            var command = CreateCommand(parameter => flag = (bool)parameter);
+
+            Assert.That(flag, Is.False);
+
+            command.Execute(true);
+
+            Assert.That(flag, Is.True);
+        }
+
+        [Test]
+        public override void Execute_IsInvokedWithParameter_WhenParameterIsPassedAndPredicateIsTrue()
+        {
+            var flag = false;
+            var command = CreateCommand(parameter =>
+            {
+                var parameters = (bool[])parameter;
+                flag = parameters[0];
+            }, parameter =>
+            {
+                var parameters = (bool[])parameter;
+                return !parameters[1];
+            });
+
+            Assert.That(flag, Is.False);
+
+            command.Execute(new[] { true, false });
+
+            Assert.That(flag, Is.True);
+        }
+#nullable enable
+        #endregion Execute
+
+        #region CanExecute
+        public override void CanExecute_ReturnsTrue_WhenThereIsNoPredicate()
+        {
+            var command = CreateCommand(parameter => { });
+
+            Assert.That(command.CanExecute(), Is.True);
+        }
+
+#nullable disable
+        [Test]
+        public override void CanExecute_ReturnsFalse_WhenPredicateIsFalse()
+        {
+            var condition = false;
+            var command = CreateCommand(parameter => { }, parameter => condition);
+
+            Assert.That(command.CanExecute(), Is.False);
+        }
+
+        [Test]
+        public override void CanExecute_ReturnsTrue_WhenPredicateIsTrue()
+        {
+            var condition = false;
+            var command = CreateCommand(parameter => { }, parameter => condition);
+
+            Assert.That(condition, Is.False);
+
+            condition = true;
+
+            Assert.That(command.CanExecute(), Is.True);
+        }
+
+        [Test]
+        public override void CanExecute_ReturnsFalse_WhenParameterIsPassedAndPredicateIsFalse()
+        {
+            var command = CreateCommand(parameter => { }, parameter => (bool)parameter);
+
+            Assert.That(command.CanExecute(false), Is.False);
+        }
+
+        [Test]
+        public override void CanExecute_ReturnsTrue_WhenParameterIsPassedAndPredicateIsTrue()
+        {
+            var command = CreateCommand(parameter => { }, parameter => (bool)parameter);
+
+            Assert.That(command.CanExecute(true), Is.True);
+        }
+#nullable enable
+        #endregion CanExecute
+
+        #region ConvertParameter
+        [Test]
+        public void ConvertParameter_ReturnsParameter_WhenParameterIsOfTypeT()
+        {
+            object parameter = "test";
+
+            var result = Command.ConvertParameter<string>(parameter);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.EqualTo(parameter));
+                Assert.That(result, Is.TypeOf<string>());
+            });
+        }
+
+        [Test]
+        public void ConvertParameter_Throws_WhenParameterIsNotOfTypeT()
+        {
+            object parameter = 1;
+
+            Assert.Throws<ArgumentException>(() => Command.ConvertParameter<string>(parameter));
+        }
+
+#nullable disable
+        [Test]
+        public void ConvertParameter_Throws_WhenParameterIsNull()
+        {
+            object parameter = null;
+
+            Assert.Throws<ArgumentNullException>(() => Command.ConvertParameter<string>(parameter));
+        }
+#nullable enable
+        #endregion ConvertParameter
+    }
+
+    public class CommandTTests : CommandTestBase<string>
+    {
+        protected override Command CreateCommand(Action<string?> execute) => new Command<string>(execute);
+
+        protected override Command CreateCommand(Action<string?> execute, Predicate<string?> canExecute) => new Command<string>(execute, canExecute);
+
+        #region Execute
+        [Test]
+        public override void Execute_IsInvoked_WhenThereIsNoPredicate()
+        {
+            var flag = false;
+            var command = CreateCommand(parameter => flag = true);
+
+            Assert.That(flag, Is.False);
+
+            command.Execute("test");
+
+            Assert.That(flag, Is.True);
+        }
+
+        [Test]
+        public override void Execute_IsNotInvoked_WhenPredicateIsFalse()
+        {
+            var flag = false;
+            var condition = false;
+            var command = CreateCommand(parameter => flag = true, parameter => condition);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(flag, Is.False);
+                Assert.That(condition, Is.False);
+            });
+
+            command.Execute("test");
+
+            Assert.That(flag, Is.False);
+        }
+
+        [Test]
+        public override void Execute_IsInvoked_WhenPredicateIsTrue()
+        {
+            var flag = false;
+            var condition = false;
+            var command = CreateCommand(parameter => flag = true, parameter => condition);
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(flag, Is.False);
+                Assert.That(condition, Is.False);
+            });
+
+            condition = true;
+            command.Execute("test");
+
+            Assert.That(flag, Is.True);
+        }
+
+#nullable disable
+        [Test]
+        public override void Execute_IsInvokedWithParameter_WhenParameterIsPassed()
+        {
+            var flag = false;
+            var command = CreateCommand(parameter => flag = parameter == "success");
+
+            Assert.That(flag, Is.False);
+
+            command.Execute("success");
+
+            Assert.That(flag, Is.True);
+        }
+
+        [Test]
+        public override void Execute_IsInvokedWithParameter_WhenParameterIsPassedAndPredicateIsTrue()
+        {
+            var flag = false;
+            var command = CreateCommand(
+                parameter => flag = parameter == "success",
+                parameter => parameter != "fail");
+
+            Assert.That(flag, Is.False);
+
+            command.Execute("success");
+
+            Assert.That(flag, Is.True);
+        }
+#nullable enable
+        #endregion Execute
+
+        #region CanExecute
+        public override void CanExecute_ReturnsTrue_WhenThereIsNoPredicate()
+        {
+            var command = CreateCommand(parameter => { });
+
+            Assert.That(command.CanExecute("test"), Is.True);
+        }
+
+#nullable disable
+        [Test]
+        public override void CanExecute_ReturnsFalse_WhenPredicateIsFalse()
+        {
+            var condition = false;
+            var command = CreateCommand(parameter => { }, parameter => condition);
+
+            Assert.That(command.CanExecute("test"), Is.False);
+        }
+
+        [Test]
+        public override void CanExecute_ReturnsTrue_WhenPredicateIsTrue()
+        {
+            var condition = false;
+            var command = CreateCommand(parameter => { }, parameter => condition);
+
+            Assert.That(condition, Is.False);
+
+            condition = true;
+
+            Assert.That(command.CanExecute("test"), Is.True);
+        }
+
+        [Test]
+        public override void CanExecute_ReturnsFalse_WhenParameterIsPassedAndPredicateIsFalse()
+        {
+            var command = CreateCommand(parameter => { }, parameter => parameter == "success");
+
+            Assert.That(command.CanExecute("fail"), Is.False);
+        }
+
+        [Test]
+        public override void CanExecute_ReturnsTrue_WhenParameterIsPassedAndPredicateIsTrue()
+        {
+            var command = CreateCommand(parameter => { }, parameter => parameter == "success");
+
+            Assert.That(command.CanExecute("success"), Is.True);
+        }
+#nullable enable
+        #endregion CanExecute
+    }
+}
---
+++ CommonUtilitiesCSharp/Core/Command.cs
@@ -0,0 +1,133 @@
+﻿namespace CommonUtilitiesCSharp.Core
+{
+    /// <summary>
+    /// Wrapper class for the execution of actions that require predicates to be met before executing.
+    /// </summary>
+    public class Command
+    {
+        #region Fields
+        private readonly Action<object?> _execute;
+        private readonly Predicate<object?> _canExecute = param => true;
+        #endregion Fields
+
+        #region Constructors
+        /// <summary>
+        /// Initializes an instance of a command that will always execute.
+        /// </summary>
+        /// <param name="execute">Action to execute when the command is triggered</param>
+        public Command(Action<object?> execute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        /// <summary>
+        /// Initializes an instance of a command that will only execute when the predicate is met.
+        /// </summary>
+        /// <param name="execute">Action to execute when the command is triggered.</param>
+        /// <param name="canExecute">Predicate that must be met for the command to execute.</param>
+        public Command(Action<object?> execute, Predicate<object?> canExecute)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute ?? throw new ArgumentNullException(nameof(canExecute));
+        }
+        #endregion Constructors
+
+        #region Members
+        /// <summary>
+        /// Returns true if the command is allowed to execute, taking into account the given parameter.
+        /// </summary>
+        /// <param name="parameter">Parameter to be used by the predicate to determine if the command can be executed.</param>
+        /// <returns>Returns true if the command is allowed to execute.</returns>
+        public bool CanExecute(object? parameter = null)
+        {
+            return _canExecute(parameter);
+        }
+
+        /// <summary>
+        /// Attempts to execute the command, using the given parameter. If the command is not allowed to execute, nothing happens.
+        /// </summary>
+        /// <param name="parameter">Parameter to be used by the predicate to determine if the command can be executed and the execution logic itself if the command is allowed to execute.</param>
+        public void Execute(object? parameter = null)
+        {
+            if (CanExecute(parameter))
+                _execute(parameter);
+        }
+        #endregion Members
+
+        /// <summary>
+        /// Converts the given parameter to the expected type and throw a standard exception if the conversion fails.
+        /// </summary>
+        /// <typeparam name="TParameter">Type to cast the given parameter to.</typeparam>
+        /// <param name="commandParameter">Parameter to cast to the designated type.</param>
+        /// <returns>Returns <paramref name="commandParameter"/> cast to the type <typeparamref name="TParameter"/>.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="commandParameter"/> is <see langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">Thrown is <paramref name="commandParameter"/> cannot be cast to type <typeparamref name="TParameter"/>.</exception>
+        public static TParameter ConvertParameter<TParameter>(object? commandParameter)
+        {
+            if (commandParameter is null)
+            {
+                throw new ArgumentNullException(nameof(commandParameter));
+            }
+
+            if (commandParameter is TParameter expectedParameter)
+            {
+                return expectedParameter;
+            }
+            else
+            {
+                throw new ArgumentException($"Parameter \"{commandParameter}\" is of type \"{commandParameter.GetType().Name}\" rather than expected type \"{typeof(TParameter).Name}\"", nameof(commandParameter));
+            }
+        }
+    }
+
+    /// <summary>
+    /// Wrapper class for the execution of actions that require predicates to be met before executing. Command paramter is automatically cast to the expected type.
+    /// </summary>
+    /// <typeparam name="TParameter">Expected command parameter type.</typeparam>
+    public class Command<TParameter> : Command
+    {
+        #region Private Methods
+        private static void ConvertExecute(Action<TParameter?> action, object? commandParameter) => action(ConvertParameter(commandParameter));
+
+        private static bool ConvertCanExecute(Predicate<TParameter?> predicate, object? commandParameter) => predicate(ConvertParameter(commandParameter));
+
+        private static TParameter? ConvertParameter(object? commandParameter) => ConvertParameter<TParameter?>(commandParameter);
+        #endregion Private Methods
+
+        #region Constructors
+        /// <inheritdoc cref="Command(Action{object?})"/>
+        public Command(Action<TParameter?> execute) : base(commandParameter => ConvertExecute(execute, commandParameter))
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute)); // Avoids propagating a forced null value down the chain
+        }
+
+        /// <inheritdoc cref="Command(Action{object?}, Predicate{object?})"/>
+        public Command(Action<TParameter?> execute, Predicate<TParameter?> canExecute) : base(
+            commandParameter => ConvertExecute(execute, commandParameter),
+            commandParameter => ConvertCanExecute(canExecute, commandParameter))
+        {
+            if (execute is null) throw new ArgumentNullException(nameof(execute)); // Avoids propagating a forced null value down the chain
+            if (canExecute is null) throw new ArgumentNullException(nameof(canExecute)); // Avoids propagating a forced null value down the chain
+        }
+        #endregion Constructors
+
+        #region Members
+        /// <inheritdoc cref="Command.CanExecute(object?)"/>
+        public bool CanExecute(TParameter? parameter)
+        {
+            return base.CanExecute(parameter);
+        }
+
+        /// <inheritdoc cref="Command.Execute(object?)"/>
+        public void Execute(TParameter? parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+
+            base.Execute(parameter);
+        }
+        #endregion Members
+    }
+}
