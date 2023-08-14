613dd82004e524fd44dc9e46c00b4c21be09dcbe
David Cardinal <david.cardinal235@gmail.com>
2023-08-14 02:09:46 -0400

    Adds the ExceptionHelper class

---
+++ CommonUtilitiesCSharp.UnitTests/Utils/ExceptionHelperTests.cs
@@ -0,0 +1,55 @@
+﻿using CommonUtilitiesCSharp.Utils;
+
+namespace CommonUtilitiesCSharp.UnitTests.Utils
+{
+    public class ExceptionHelperTests
+    {
+        #region CastValue
+        [Test]
+        public void CastValue_ReturnsValue_ForValidValue()
+        {
+            object value = "test";
+            
+            var result = ExceptionHelper.CastValue<string>(value, "parameter");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.EqualTo(value));
+                Assert.That(result, Is.TypeOf<string>());
+            });
+        }
+
+        [Test]
+        public void CastValue_ReturnsValue_ForInterfaceCasting()
+        {
+            object value = new List<string>();
+
+            object result = ExceptionHelper.CastValue<IEnumerable<string>>(value, "parameter");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.EqualTo(value));
+                Assert.That(result is IEnumerable<string>);
+            });
+        }
+
+        [Test]
+        public void CastValue_Throws_ForInvalidValue()
+        {
+            object value = 1;
+
+            Assert.Throws<ArgumentException>(() => ExceptionHelper.CastValue<string>(value, "parameter"));
+        }
+
+#nullable disable
+        [Test]
+        public void CastValue_Throws_ForNullValue()
+        {
+            object value = null;
+
+            Assert.Throws<ArgumentException>(() => ExceptionHelper.CastValue<string>(value, "parameter"));
+        }
+#nullable enable
+        #endregion CastValue
+    }
+}
---
+++ CommonUtilitiesCSharp/Utils/ExceptionHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace CommonUtilitiesCSharp.Utils
+{
+    /// <summary>
+    /// Helper class containing methods for interacting with exceptions.
+    /// </summary>
+    public static class ExceptionHelper
+    {
+        /// <summary>
+        /// Casts <paramref name="value"/> to target type <typeparamref name="TTargetType"/> and throws a standardized <see cref="ArgumentNullException"/> if the value cannot be cast to the expected type.
+        /// </summary>
+        /// <typeparam name="TTargetType">Type to cast the <paramref name="value"/> to.</typeparam>
+        /// <param name="value">Value to be cast</param>
+        /// <param name="paramName">Parameter name of the given value. (to be used in the <see cref="ArgumentException"/> message)</param>
+        /// <param name="paramDisplayName">Display name for the parameter of the given value. (to be used in the <see cref="ArgumentException"/> message)</param>
+        /// <returns><paramref name="value"/> cast as type <typeparamref name="TTargetType"/>.</returns>
+        /// <exception cref="ArgumentException">Thrown if <paramref name="value"/> cannot be cast to the type <typeparamref name="TTargetType"/>.</exception>
+        public static TTargetType CastValue<TTargetType>(object? value, string paramName, string? paramDisplayName = null)
+        {
+            if (value is TTargetType target)
+            {
+                return target;
+            }
+
+            throw new ArgumentException($"{paramDisplayName ?? paramName} of type \"{value?.GetType().Name ?? "null"}\" is not of expected type \"{typeof(TTargetType).Name}\"", paramName);
+        }
+    }
+}
