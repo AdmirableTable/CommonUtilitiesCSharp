c5cfb40bb13724363729b797c059309b931f6e73
David Cardinal <david.cardinal235@gmail.com>
2023-08-14 05:17:42 -0400

    Adds the HashCodeHelper class

---
+++ CommonUtilitiesCSharp.UnitTests/Utils/HashCodeHelperTests.cs
@@ -0,0 +1,157 @@
+﻿using CommonUtilitiesCSharp.Utils;
+
+namespace CommonUtilitiesCSharp.UnitTests.Utils
+{
+    public class HashCodeHelperTests
+    {
+        #region CombineSequence
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValues()
+        {
+            var hash1 = HashCodeHelper.CombineSequence(1, 2, 3);
+            var hash2 = HashCodeHelper.CombineSequence(1, 2, 3);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsDifferentHashForDifferentValues()
+        {
+            var hash1 = HashCodeHelper.CombineSequence(1, 2, 3);
+            var hash2 = HashCodeHelper.CombineSequence(1, 2, 4);
+
+            Assert.That(hash1, Is.Not.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValuesWithDifferentOrder()
+        {
+            var hash1 = HashCodeHelper.CombineSequence(1, 2, 3);
+            var hash2 = HashCodeHelper.CombineSequence(3, 2, 1);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValuesWithDuplicates()
+        {
+            var hash1 = HashCodeHelper.CombineSequence(1, 2, 3, 3);
+            var hash2 = HashCodeHelper.CombineSequence(1, 2, 3, 3);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsDifferentHashForIdenticalValuesWithDuplicates()
+        {
+            var hash1 = HashCodeHelper.CombineSequence(1, 2, 3 ,3);
+            var hash2 = HashCodeHelper.CombineSequence(1, 2 ,3);
+
+            Assert.That(hash1, Is.Not.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValuesWithNulls()
+        {
+            var hash1 = HashCodeHelper.CombineSequence<int?>(1, 2, 3, null);
+            var hash2 = HashCodeHelper.CombineSequence<int?>(1, 2, 3, null);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValuesWithDifferentOrderAndNulls()
+        {
+            var hash1 = HashCodeHelper.CombineSequence<int?>(1, 2, 3, null);
+            var hash2 = HashCodeHelper.CombineSequence<int?>(3, 2, 1, null);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValuesWithDuplicatesAndNulls()
+        {
+            var hash1 = HashCodeHelper.CombineSequence<int?>(1, 2, 3, 3, null);
+            var hash2 = HashCodeHelper.CombineSequence<int?>(1, 2, 3, 3, null);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValuesWithDifferentTypes()
+        {
+            var hash1 = HashCodeHelper.CombineSequence<object>(1, 2, 3, "4");
+            var hash2 = HashCodeHelper.CombineSequence<object>(1, 2, 3, "4");
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalValuesOfNonComparableTypes()
+        {
+            var arr1 = new[] { 1 };
+            var arr2 = new[] { 2 };
+            var arr3 = new[] { 3 };
+
+            var hash1 = HashCodeHelper.CombineSequence(arr1, arr2, arr3);
+            var hash2 = HashCodeHelper.CombineSequence(arr1, arr2, arr3);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsDifferentHashForIdenticalValuesOfNonComparableTypesWithDifferentOrder()
+        {
+            var arr1 = new[] { 1 };
+            var arr2 = new[] { 2 };
+            var arr3 = new[] { 3 };
+
+            var hash1 = HashCodeHelper.CombineSequence(arr1, arr2, arr3);
+            var hash2 = HashCodeHelper.CombineSequence(arr3, arr2, arr1);
+
+            Assert.That(hash1, Is.Not.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForEmptyCollections()
+        {
+            var hash1 = HashCodeHelper.CombineSequence(Array.Empty<int>());
+            var hash2 = HashCodeHelper.CombineSequence(Array.Empty<int>());
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsDifferentHashForEmptyCollectionsOfDifferentTypes()
+        {
+            var hash1 = HashCodeHelper.CombineSequence(Array.Empty<int>());
+            var hash2 = HashCodeHelper.CombineSequence(Array.Empty<object>());
+
+            Assert.That(hash1, Is.Not.EqualTo(hash2));
+        }
+
+        [Test]
+        public void CombineSequence_ThrowsForSingleCollection()
+        {
+            var values = new List<int>() { 1, 2, 3 };
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                HashCodeHelper.CombineSequence(values);
+                // Expected and required usage is HashCodeHelper.CombineSequence<int>(values);
+            });
+        }
+
+        [Test]
+        public void CombineSequence_ReturnsSameHashForIdenticalCollections()
+        {
+            var values1 = new List<int>() { 1, 2, 3 };
+            var values2 = new List<int>() { 1, 2, 3 };
+
+            var hash1 = HashCodeHelper.CombineSequence<int>(values1);
+            var hash2 = HashCodeHelper.CombineSequence<int>(values2);
+
+            Assert.That(hash1, Is.EqualTo(hash2));
+        }
+        #endregion CombineSequence
+    }
+}
---
+++ CommonUtilitiesCSharp/Utils/HashCodeHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+
+namespace CommonUtilitiesCSharp.Utils
+{
+    /// <summary>
+    /// Helper class containing methods to assist with hash codes.
+    /// </summary>
+    public static class HashCodeHelper
+    {
+        /// <summary>
+        /// Combines a set of values into a single hash code. The order of the values does not matter if the values implement IComparable. Otherwise, it will be required for a matching hash.<para/>
+        /// Empty sets of values of type <typeparamref name="T"/> will all have the same hash.
+        /// </summary>
+        /// <typeparam name="T">The type of value to add to the hash.</typeparam>
+        /// <param name="values">Set of values that will be added to the hash.</param>
+        /// <returns>A hash code unique to a set of values.</returns>
+        public static int CombineSequence<T>(IEnumerable<T> values)
+        {
+            if (!values.Any())
+                return typeof(T).GetHashCode() * nameof(HashCodeHelper).GetHashCode(); // Custom hash code to differentiate empty sets of values by their type.
+
+            var hash = new HashCode();
+            IEnumerable<T> valueList = values.ToList();
+
+            // TODO Find a way to order types that don't implement IComparable.
+            var nullableType = Nullable.GetUnderlyingType(typeof(T));
+            var valueType = nullableType ?? typeof(T);
+            if (typeof(IComparable).IsAssignableFrom(valueType))
+                valueList = valueList.OrderBy(item => item);
+
+            foreach (var value in valueList)
+                hash.Add(value);
+
+            return hash.ToHashCode();
+        }
+
+        /// <inheritdoc cref="CombineSequence{T}(IEnumerable{T})" />
+        /// <exception cref="InvalidOperationException">Thrown to avoid accidental misuse of the method, when the given argument array contains a single enumerable of values, whereas it would be expected to combine the values within that enumerable instead.</exception>
+        public static int CombineSequence<T>(params T[] values)
+        {
+            if (values.Length == 1 && values is IEnumerable) // This is not ideal and goes against some conventions, but it's the best way to avoid accidental misuse.
+                throw new InvalidOperationException($"Method cannot be used with a single {nameof(IEnumerable)} parameter to avoid misuse. The generic type {nameof(T)} must be specified manually.");
+
+            return CombineSequence(values.AsEnumerable());
+        }
+    }
+}
