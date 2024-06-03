4d8a1967aff3ac318ba5d0ba16d3d80b274429d5
David Cardinal <david.cardinal235@gmail.com>
2024-06-03 08:52:20 -0400

    Adds extension methods for CultureInfo

---
+++ CommonUtilitiesCSharp.UnitTests/Extensions/CultureInfoExtensionTests.cs
@@ -0,0 +1,44 @@
+﻿using CommonUtilitiesCSharp.Extensions;
+using System.Globalization;
+
+namespace CommonUtilitiesCSharp.UnitTests.Extensions
+{
+    public class CultureInfoExtensionTests
+    {
+        #region Is24HTimeFormat
+        [Test]
+        public void Is24HTimeFormat_ThrowsArgumentNullException_ForNull()
+        {
+            CultureInfo cultureInfo = null!;
+
+            Assert.Throws<ArgumentNullException>(() => cultureInfo.Is24HTimeFormat());
+        }
+
+        [Test]
+        public void Is24HTimeFormat_ReturnsTrue_For24HTimeFormat()
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            cultureInfo.DateTimeFormat.ShortTimePattern = "HH:mm";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 13, 0, 0).ToString("t", cultureInfo), Is.EqualTo("13:00"));
+                Assert.That(cultureInfo.Is24HTimeFormat(), Is.True);
+            });
+        }
+
+        [Test]
+        public void Is24HTimeFormat_ReturnsFalse_For12HTimeFormat()
+        {
+            var cultureInfo = new CultureInfo("en-US");
+            cultureInfo.DateTimeFormat.ShortTimePattern = "h:mm tt";
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 13, 0, 0).ToString("t", cultureInfo), Is.EqualTo("1:00 PM"));
+                Assert.That(cultureInfo.Is24HTimeFormat(), Is.False);
+            });
+        }
+        #endregion Is24HTimeFormat
+    }
+}
---
+++ CommonUtilitiesCSharp/Extensions/CultureInfoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace CommonUtilitiesCSharp.Extensions
+{
+    /// <summary>
+    /// Static class containing extension methods for <see cref="CultureInfo"/>.
+    /// </summary>
+    public static class CultureInfoExtensions
+    {
+        /// <summary>
+        /// Determines if the <see cref="CultureInfo"/> uses a 24-hour time format or a 12-hour time format.
+        /// </summary>
+        /// <param name="cultureInfo"><see cref="CultureInfo"/> to evaluate.</param>
+        /// <returns>Returns true if the <see cref="CultureInfo"/> uses a 24-hour time format.</returns>
+        /// <exception cref="ArgumentNullException">Thrown if <paramref name="cultureInfo"/> is null.</exception>
+        public static bool Is24HTimeFormat(this CultureInfo cultureInfo)
+        {
+            if (cultureInfo is null)
+            {
+                throw new ArgumentNullException(nameof(cultureInfo));
+            }
+
+            return cultureInfo.DateTimeFormat.ShortTimePattern.Contains('H');
+        }
+    }
+}
