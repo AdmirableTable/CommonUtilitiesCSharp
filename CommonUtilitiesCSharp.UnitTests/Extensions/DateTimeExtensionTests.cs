50a36432a4b1b18d73c35744e581da1f690b22bc
David Cardinal <david.cardinal235@gmail.com>
2025-11-13 18:13:47 -0500

    Adds DateTimeExtensions

---
+++ CommonUtilitiesCSharp.UnitTests/Extensions/DateTimeExtensionTests.cs
@@ -0,0 +1,181 @@
+﻿using CommonUtilitiesCSharp.Extensions;
+
+namespace CommonUtilitiesCSharp.UnitTests.Extensions
+{
+    public class DateTimeExtensionTests
+    {
+        #region SetHour
+        [Test]
+        public void SetHours_ReturnsInstanceWithNewHour_ForEarlierHour()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetHour(7);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 7, 30, 30)));
+        }
+
+        [Test]
+        public void SetHours_ReturnsInstanceWithNewHour_ForLaterHour()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetHour(11);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 11, 30, 30)));
+        }
+
+        [Test]
+        public void SetHours_ReturnsInstanceWithNewHour_ForMidnight()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetHour(0);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 0, 30, 30)));
+        }
+
+        [Test]
+        public void SetHours_ReturnsInstanceWithNewHour_For23h()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetHour(23);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 23, 30, 30)));
+        }
+
+        [Test]
+        public void SetHours_Throws_ForNegativeHour()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => dateTime.SetHour(-1));
+        }
+
+        [Test]
+        public void SetHours_Throws_ForOver23h()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => dateTime.SetHour(24));
+        }
+        #endregion SetHour
+
+        #region SetMinute
+        [Test]
+        public void SetMinutes_ReturnsInstanceWithNewMinute_ForEarlierMinute()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetMinute(15);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 15, 30)));
+        }
+
+        [Test]
+        public void SetMinutes_ReturnsInstanceWithNewMinute_ForLaterMinute()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetMinute(45);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 45, 30)));
+        }
+
+        [Test]
+        public void SetMinutes_ReturnsInstanceWithNewMinute_ForZero()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetMinute(0);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 00, 30)));
+        }
+
+        [Test]
+        public void SetMinutes_ReturnsInstanceWithNewMinute_For59m()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetMinute(59);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 59, 30)));
+        }
+
+        [Test]
+        public void SetMinutes_Throws_ForNegativeMinute()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => dateTime.SetMinute(-1));
+        }
+
+        [Test]
+        public void SetMinutes_Throws_ForOver59m()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => dateTime.SetMinute(60));
+        }
+        #endregion SetMinute
+
+        #region SetSeconds
+        [Test]
+        public void SetSeconds_ReturnsInstanceWithNewSeconds_ForEarlierSeconds()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetSeconds(15);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 30, 15)));
+        }
+
+        [Test]
+        public void SetSeconds_ReturnsInstanceWithNewSeconds_ForLaterSeconds()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetSeconds(45);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 30, 45)));
+        }
+
+        [Test]
+        public void SetSeconds_ReturnsInstanceWithNewSeconds_ForZero()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetSeconds(0);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 30, 00)));
+        }
+
+        [Test]
+        public void SetSeconds_ReturnsInstanceWithNewSeconds_For59s()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            var result = dateTime.SetSeconds(59);
+
+            Assert.That(result, Is.EqualTo(new DateTime(2025, 11, 01, 10, 30, 59)));
+        }
+
+        [Test]
+        public void SetSeconds_Throws_ForNegativeSeconds()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => dateTime.SetSeconds(-1));
+        }
+
+        [Test]
+        public void SetSeconds_Throws_ForOver59s()
+        {
+            var dateTime = new DateTime(2025, 11, 01, 10, 30, 30);
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => dateTime.SetSeconds(60));
+        }
+        #endregion SetSeconds
+    }
+}
---
+++ CommonUtilitiesCSharp/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,68 @@
+﻿namespace CommonUtilitiesCSharp.Extensions
+{
+    /// <summary>
+    /// Static class containing extension methods for <see cref="DateTime"/>.
+    /// </summary>
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Returns a new <see cref="DateTime"/> as a copy of this instance, where the hour is set to the specified number.
+        /// </summary>
+        /// <param name="dateTime">Source <see cref="DateTime"/> instance.</param>
+        /// <param name="hour">Target hour to set.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="hour"/> is not between 0 and 23.</exception>
+        /// <returns>Returns a new <see cref="DateTime"/> as a copy of this instance, where the hour is set to the specified number.</returns>
+        public static DateTime SetHour(this DateTime dateTime, int hour)
+        {
+            if (hour < 0 || hour > 23)
+            {
+                throw new ArgumentOutOfRangeException(nameof(hour), $"{nameof(hour)} must be between 0 and 23.");
+            }
+
+            var currentHours = dateTime.Hour;
+            var resultHours = hour - currentHours;
+
+            return dateTime.AddHours(resultHours);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="DateTime"/> as a copy of this instance, where the minute is set to the specified number.
+        /// </summary>
+        /// <param name="dateTime">Source <see cref="DateTime"/> instance.</param>
+        /// <param name="minute">Target minute to set.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="minute"/> is not between 0 and 59.</exception>
+        /// <returns>Returns a new <see cref="DateTime"/> as a copy of this instance, where the minute is set to the specified number.</returns>
+        public static DateTime SetMinute(this DateTime dateTime, int minute)
+        {
+            if (minute < 0 || minute > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(minute), $"{nameof(minute)} must be between 1 and 59.");
+            }
+
+            var currentMinute = dateTime.Minute;
+            var resultMinute = minute - currentMinute;
+
+            return dateTime.AddMinutes(resultMinute);
+        }
+
+        /// <summary>
+        /// Returns a new <see cref="DateTime"/> as a copy of this instance, where the seconds are set to the specified number.
+        /// </summary>
+        /// <param name="dateTime">Source <see cref="DateTime"/> instance.</param>
+        /// <param name="seconds">Target seconds to set.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown if <paramref name="seconds"/> is not between 1 and 59.</exception>
+        /// <returns>Returns a new <see cref="DateTime"/> as a copy of this instance, where the seconds are set to the specified number.</returns>
+        public static DateTime SetSeconds(this DateTime dateTime, int seconds)
+        {
+            if (seconds < 0 || seconds > 59)
+            {
+                throw new ArgumentOutOfRangeException(nameof(seconds), $"{nameof(seconds)} must be between 1 and 59.");
+            }
+
+            var currentSeconds = dateTime.Second;
+            var resultSeconds = seconds - currentSeconds;
+
+            return dateTime.AddSeconds(resultSeconds);
+        }
+    }
+}
